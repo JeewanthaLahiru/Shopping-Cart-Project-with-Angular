ac7ddbdcc52e51504658d0c65b51662a81fc0aac
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-02-28 16:47:43 +0530

    created header area components

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientAppComponent } from './client-app/client-app.component';
+import { TopHeaderComponent } from './components/headerArea/top-header/top-header.component';
+import { BottomHeaderComponent } from './components/headerArea/bottom-header/bottom-header.component';
+import { NavigationBarComponent } from './components/headerArea/navigation-bar/navigation-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClientAppComponent
+    ClientAppComponent,
+    TopHeaderComponent,
+    BottomHeaderComponent,
+    NavigationBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
